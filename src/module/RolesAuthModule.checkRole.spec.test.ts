e78d2f8e2e69f7adbb47ed64048b5196957a3c58
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-18 22:22:59 +0200

    test: test checkRole epic in RolesAuthModule

--- src/module/RolesAuthModule.checkRole.spec.test.ts
+++ src/module/RolesAuthModule.checkRole.spec.test.ts
@@ -1,19 +1,94 @@
 import * as _ from "lodash";
 
-import { AuthState } from "../AuthState";
-
 import { PlainActions } from "./actions/PlainActions";
-import { getSampleFirebaseAccount, mock } from "./RolesAuthModule.mocks.test";
+import { expectActionDispatched, mock } from "./RolesAuthModule.mocks.test";
 
 describe("RolesAuthModule", () => {
-    describe.skip("#checkRole", () => {
-        it("Fails on invalid role");
-        it("Calls rolesAdapter.hasRole");
-        it("Sets role key in .roles");
-        it("Calls rolesAdapter.isRoleRequestedByUser");
-        it("Sets role request key in .roleRequests");
+    describe("#checkRole", () => {
+        const sampleAccountUid = `${Math.random()}`;
+        const role = "admin";
+        let m: ReturnType<typeof mock>;
+        beforeEach(async () => {
+            m = mock({ rolesAuth: { account: { uid: sampleAccountUid } } });
+            m.rolesAdapter.hasRole = jest.fn(async (uid: string, roleName: string) => true);
+            m.rolesAdapter.isRoleRequestedByUser = jest.fn(async (uid: string, roleName: string) => true);
+        });
 
-        it("On error in rolesAdapter.hasRole calls callbacks.onError");
-        it("On error in rolesAdapter.isRoleRequestedByUser calls callbacks.onError");
+        /**
+         * Domain language
+         */
+        const dispatchCheckRoleInMock = () => m.storeMock.dispatch<any>(m.raModule.actions.checkRole(role));
+        const dispatchCheckRoleInStore = () => m.store.dispatch<any>(m.raModule.actions.checkRole(role));
+
+        /**
+         * Tests
+         */
+        /*it("Dispatches setRoleStatus action", async () => {
+            await dispatchCheckRoleInMock();
+            expectActionDispatched(m, PlainActions.SET_ROLE_STATUS);
+        });
+
+        it("Dispatches setRoleRequestStatus action", async () => {
+            await dispatchCheckRoleInMock();
+            expectActionDispatched(m, PlainActions.SET_ROLE_REQUEST_STATUS);
+        });*/
+
+        it("Calls rolesAdapter.hasRole", async () => {
+            await dispatchCheckRoleInStore();
+            expect(m.rolesAdapter.hasRole).toBeCalledTimes(1);
+            expect(m.rolesAdapter.hasRole).toBeCalledWith(sampleAccountUid, role);
+        });
+
+        it("Sets role key in .roles", async () => {
+            await dispatchCheckRoleInStore();
+            expect(m.store.getState().rolesAuth.roles[role]).toEqual(true);
+        });
+
+        [true, false].forEach(hasRole =>
+            describe("hasRole=" + hasRole, () => {
+                beforeEach(() => {
+                    m.rolesAdapter.hasRole = jest.fn(async (uid: string, roleName: string) => hasRole);
+                });
+
+                it(
+                    (hasRole ? "Does not call" : "Calls") +
+                        " rolesAdapter.isRoleRequestedByUser when hasRole = " +
+                        hasRole,
+                    async () => {
+                        await dispatchCheckRoleInStore();
+
+                        expect(m.rolesAdapter.isRoleRequestedByUser).toBeCalledTimes(hasRole ? 0 : 1);
+                        if (!hasRole) {
+                            expect(m.rolesAdapter.isRoleRequestedByUser).toBeCalledWith(sampleAccountUid, role);
+                        }
+                    },
+                );
+
+                it(
+                    (hasRole ? "Does not set" : "Sets") +
+                        " role request key in .roleRequests when hasRole = " +
+                        hasRole,
+                    async () => {
+                        await dispatchCheckRoleInStore();
+                        if (!hasRole) {
+                            expect(m.store.getState().rolesAuth.roleRequests[role]).toEqual(true);
+                        } else {
+                            expect(m.store.getState().rolesAuth.roleRequests).not.toHaveProperty(role);
+                        }
+                    },
+                );
+            }),
+        );
+
+        ["hasRole", "isRoleRequestedByUser"].forEach(method =>
+            it(`On error in rolesAdapter.${method} calls callbacks.onError`, async () => {
+                m.rolesAdapter.hasRole = jest.fn(async (uid: string, roleName: string) => false);
+                (m.rolesAdapter as any)[method] = jest.fn((uid: string, roleName: string) => {
+                    throw new Error("Sample error");
+                });
+                await dispatchCheckRoleInStore();
+                expect(m.callbacks.onError).toBeCalledTimes(1);
+            }),
+        );
     });
 });
