2544b861941897b6c685b2be34821ffa13449959
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-18 20:55:02 +0200

    fix: fix reducer not reseting roles and roleRequests after authentication or logout

--- src/module/reducer.ts
+++ src/module/reducer.ts
@@ -7,6 +7,7 @@ import { AuthState } from "../AuthState";
 import { PlainActions } from "./actions/PlainActions";
 import { State } from "./State";
 import { RootAction } from "./types";
+import { EpicActions } from "./actions/EpicActions";
 
 export function configureReducer() {
     const authState = createReducer<AuthState>(AuthState.NOTAUTHENTICATED) //
@@ -19,12 +20,20 @@ export function configureReducer() {
         .handleAction(PlainActions.Actions.authSuccess, (state, action) => action.payload);
 
     const roles = createReducer<{ [roleName: string]: boolean }>({}) //
+        .handleAction(
+            [PlainActions.Actions.authNotAuthenticated, PlainActions.Actions.authSuccess],
+            (state, action) => ({}),
+        )
         .handleAction(PlainActions.Actions.setRoleStatus, (state, action) => ({
             ...state,
             [action.payload.role]: action.payload.hasRole,
         }));
 
     const roleRequests = createReducer<{ [roleName: string]: boolean }>({}) //
+        .handleAction(
+            [PlainActions.Actions.authNotAuthenticated, PlainActions.Actions.authSuccess],
+            (state, action) => ({}),
+        )
         .handleAction(PlainActions.Actions.setRoleRequestStatus, (state, action) => ({
             ...state,
             [action.payload.role]: action.payload.isRequestingRole,
